d756f7de0f103e18b8e98d0c670c363710661b12
Denis Bubnov <denisbubnow@mail.ru>
2023-02-03 13:19:42 +0300

    Тестирование долбаного CSRF токена

---
+++ src/app/app.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  
+  title = 'csrf-web-client';
+
+  getCookie(name: string): string | null {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+
+    return cookieValue;
+  }
+
+  onSave() {
+    
+    fetch('http://127.0.0.1/api/v1', {
+      method: 'GET',
+      credentials: 'include',
+      cache: 'no-cache',
+      mode: 'cors'
+    })
+    .then((response) => {
+
+      response
+        .json()
+        .then(res => {
+          console.log('First result:', res);
+        });
+
+      const csrftoken = this.getCookie('csrftoken');
+      console.log('csrftoken:', csrftoken);
+
+      if (csrftoken) {
+
+        fetch('http://127.0.0.1/api/v1/auth/login', {
+          method: 'POST',
+          credentials: 'include',
+          cache: 'no-cache',
+          mode: 'cors',
+          headers: {
+            'X-CSRFToken': csrftoken
+          }
+        })
+        .then((response) => {
+            response
+              .json()
+              .then(res => {
+                console.log('Second result:', res);
+              });
+        })
+        .catch((error) => {
+          console.log('Cannot execute POST method. Error:', error);
+        });
+
+      }  
+    })
+    .catch((error) => {
+      console.log('Cannot execute GET method. Error:', error);
+    });
+  }
+}
