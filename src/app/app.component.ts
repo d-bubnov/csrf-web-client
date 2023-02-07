724f36bdf90ec50ffd588155aeeb602878c959d4
Denis Bubnov <denisbubnow@mail.ru>
2023-02-07 14:00:15 +0300

    Засетить куки в документ и использовать include при отправке запросов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,81 +2,94 @@ import { Component } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { getCookie } from 'src/app/helpers/cookieHelper';
 
+interface TokenResponse {
+  csrf_token: string;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
 
   constructor(private cookieService: CookieService) {}
 
   onCsrfTokenClick() {
-    
-    fetch('http://127.0.0.1:8000/api/v1/get-csrf-token/', {
-      method: 'GET',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      mode: 'no-cors',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-    .then((response) => {
-        const csrftoken = getCookie('csrftoken');
-        console.log('Call `api/v1/get-csrf-token` API. Value of the csrftoken:', csrftoken);
-
-        const csrftokenApi = this.cookieService.get('csrftoken');
-        console.log('Call `api/v1/get-csrf-token` API. Value of the csrftoken (from service):', csrftokenApi);
-
-        this.fetchApi();
-    });
+    // Получение CSRF токена из эндпоинта:
+    this.getCsrfToken();
   }
 
-  fetchApi(){
-
-    fetch('http://127.0.0.1:8000/api/v1/', {
+  getCsrfToken() {
+    fetch('http://127.0.0.1:8000/api/v1/get-csrf-token/', {
       method: 'GET',
-      credentials: 'include',
-      cache: 'no-cache',
-      mode: 'no-cors',
+      credentials: 'same-origin',
+      mode: 'cors',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
     })
-    .then((response) => {
+    .then(response => {
+      return response.json();
+    })
+    .then((token: TokenResponse) => {
+      if (token && token.csrf_token) {
+        // Засетим токен в наши куки:
+        document.cookie = `csrftoken=${token.csrf_token}`;
+
+        console.log('csrftoken', token.csrf_token);
+
+        this.getEndpoints(token.csrf_token);
+        this.authLogin(token.csrf_token);
+      }
     })
     .catch((error) => {
       console.log('Cannot execute GET method. Error:', error);
     });
+  }
 
+  getEndpoints(csrfToken: string) {
+    // Получение эндпоинтов методом GET:
+    fetch('http://127.0.0.1:8000/api/v1/', {
+      method: 'GET',
+      credentials: 'include',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'x-csrftoken': csrfToken,
+      },
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then((endpoints) => {
+      // console.log('Endpoints', endpoints);
+    })
+    .catch((error) => {
+      console.log('Cannot execute GET method. Error:', error);
+    });
+  }
 
-    const csrftoken = getCookie('csrftoken');
+  authLogin(csrfToken: string) {
 
     fetch('http://127.0.0.1:8000/api/v1/api-auth/login/', {
       method: 'POST',
       credentials: 'include',
-      cache: 'no-cache',
-      mode: 'no-cors',
+      mode: 'cors',
       headers: {
-        'HTTP_X_CSRFTOKEN': `${csrftoken}`
+        Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'x-csrftoken': csrfToken,
+          'Authorization': 'Basic admin:admin'
       },
-      body: JSON.stringify({
-        username: 'admin',
-        password: 'admin',
-      }),
     })
     .then((response) => {
-      const csrftoken = getCookie('csrftoken');
-      console.log('Call `api/v1/api-auth/login` API. Value of the csrftoken:', csrftoken);
+      // console.log('api/v1/api-auth/login', response);
     })
     .catch((error) => {
       console.log('Cannot execute POST method. Error:', error);
     });
-
-
   }
 }
